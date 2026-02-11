4ca4380eba02d5c10186abb7c2f83b6b0680f0ab
Olli <ollilain@gmail.com>
2026-02-11 15:10:37 +0200

    add hanno project and edit header and styles

--- modules/header.js
+++ modules/header.js
@@ -1,20 +1,35 @@
-export function initHeader(pageType) {
+export function initHeader(pageType, projectName) {
     const headerDiv = document.getElementById("header");
     if (headerDiv) {
-        // create upper header
+        // create logo div 
         const logoHeaderDiv = document.createElement("div");
         logoHeaderDiv.innerHTML = "<h1>Vinkkeli</h1>";
         logoHeaderDiv.setAttribute("class", "header-logo");
         headerDiv.appendChild(logoHeaderDiv); 
 
-        // create lower header
+       /* 
+        // create project title
+        if (pageType === "project") {
+            const projectTitleDiv = document.createElement("div");
+            projectTitleDiv.innerHTML = `<h1>${projectName}</h1>`;
+            projectTitleDiv.setAttribute("class", "header-title");
+            headerDiv.append(projectTitleDiv);
+        }
+        */
+
+        // create menu div
         const menuHeaderDiv = document.createElement("div");
-	if (pageType === "index") {
-        menuHeaderDiv.innerHTML = '<a href="contact.html">Yrityksestä</a>';
-	} else if (pageType === "contact") {
-        menuHeaderDiv.innerHTML = '<a href="index.html">Etusivu</a>';
-    }
+
+        if (pageType === "index") {
+            menuHeaderDiv.innerHTML = '<a href="contact.html">Yrityksestä</a>';
+	    } else if (pageType === "contact") {
+            menuHeaderDiv.innerHTML = '<a href="index.html">Etusivu</a>';
+        } else if (pageType === "project") {
+            menuHeaderDiv.innerHTML = '<a href="index.html">Etusivu</a><p>/</p><a href="contact.html">Yrityksestä</a>';
+        }
         menuHeaderDiv.setAttribute("class", "header-menu");
         headerDiv.append(menuHeaderDiv);
+
+
     }
 }
